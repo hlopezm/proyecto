10653e1f168183f47902f313adef884b6c16c4b9
hlopez <hlopezm23@gmail.com>
2015-02-09 23:46:21 -0600

    sessions and main features is ready, now debuggin

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
-  validates_presence_of :name, :price
+  validates_presence_of :name
   validates_numericality_of :price
 
   belongs_to :category
